0fa86f4ce191d57822c2c2e66fd4e0a0f68d91b9
zart227 <39973078+zart227@users.noreply.github.com>
2024-05-30 21:59:37 +0300

    Update articleModel.js

--- backend/models/articleModel.js
+++ backend/models/articleModel.js
@@ -5,7 +5,7 @@ const { ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const url = process.env.MONGO_URI || "mongodb://localhost:27017";
-const dbName = process.env.DB_NAME || "articles";
+const dbName = process.env.DB_NAME || "pollutionData";
 
 async function connectToDatabase() {
   const client = new MongoClient(url, {
